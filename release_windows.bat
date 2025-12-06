ac02223409eee76312925de24021543996a3dba1
DivingDuck <DivingDuck@users.noreply.github.com>
2025-12-06 21:37:24 +0100

    Remove workaround for regression in pyinstaller with pefile != 2024.8.26. this is solved with the actual version 6.17.0 of pyinstaller. This applies only local windows builds with script release_windows.bat

--- release_windows.bat
+++ release_windows.bat
@@ -138,8 +138,10 @@ rem --exclude pefile: workaround, (Windows) Pin pefile != 2024.8.26 due to perfo
 rem              regression in pefile 2024.8.26 that heavily impacts PyInstaller’s
 rem              binary dependency analysis and binary-vs-data classification. See
 rem              https://pyinstaller.org/en/stable/CHANGES.html#pyinstaller-core
-for /F "skip=2 delims= " %%i in ('pip list --outdated --exclude pyglet --exclude pefile') do py -m pip install --upgrade %%i
-
+rem              # Update 2025-12-06: this seems to be solved now with pyinstaller v6.17.0 (2025-11-24).
+rem                See https://pyinstaller.org/en/stable/CHANGES.html#bugfix
+rem for /F "skip=2 delims= " %%i in ('pip list --outdated --exclude pyglet --exclude pefile') do py -m pip install --upgrade %%i
+for /F "skip=2 delims= " %%i in ('pip list --outdated --exclude pyglet') do py -m pip install --upgrade %%i
 
 rem echo *************************************************************************
 rem echo ****** pyglet workaround, needs to be below 2.0 (isn't compatible) ******
